6f549b600f10e12e1c4f90659fc62ddfb39a73fb
ja1423 <aziabdujabborova1423@gmail.com>
2024-08-12 19:50:35 +0500

    .

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,14 @@ import Vuex from 'vuex';
 
 import { userModule } from './modules/user.module.js';
 import { stayModule } from './modules/stay.module.js';
-import { orderModule } from './modules/order.module.JS';
+// import { orderModule } from './modules/order.module.JS';
 
 export const store = Vuex.createStore({
   strict: true,
   modules: {
     userModule,
     stayModule,
-    orderModule,
+    // orderModule,
   },
   state: {},
   mutations: {},
